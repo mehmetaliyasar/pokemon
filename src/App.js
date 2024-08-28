0973ee1b879a84c03927f4ae570cd0023d272fe8
34rbn <mimarmehmetali35@gmail.com>
2024-08-28 18:13:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './Pokecard.css';
+
+import Pokegame from './Pokegame';
+
+
+function App() {
+  return (
+    <div className="App">
+  <h1>
+    pokemon go card game
+  </h1>
+ 
+  <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
